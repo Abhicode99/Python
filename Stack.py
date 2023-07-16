8fa4ca9a27719ad809d462129f1e405fd4bab0a0
Abhicode99 <67646779+Abhicode99@users.noreply.github.com>
2023-07-16 19:34:57 +0530

    Add files via upload

---
+++ Stack.py
@@ -0,0 +1,29 @@
+
+
+class Stack:
+    def __init__(self):
+        self.items=[2,3.5]
+        print('This step executed')
+
+    def is_empty(self):
+        return not self.items
+
+    def push(self,item):
+        self.items.append(item)
+
+    def pop(self):
+        self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+    def __str(self):
+        return str(self.items)
+
+#if __name__ == "__main__" :
+ #   s = Stack()
+    #print(s)'''
+
